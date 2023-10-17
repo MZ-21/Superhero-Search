9b4264d788aaf25ecaa3716a954c373f17e85fca
mzekria2 <mzekria2@uwo.ca>
2023-10-17 15:27:41 -0400

    added parts

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express()
 const port = 3000; 
 
+
+//temporary database
+const parts = [
+    {id:100, name:"Belt",colour: "brown"},
+    {id:101, name:"Clip",colour: "brown"},
+    {id:102, name:"Belt",colour: "red"},
+    {id:103, name:"Hat",colour: "purple"},
+];
 //using app.get to get url so when someone access that path, something happens
-app.get('/',(req,res)=>{
-    res.send("Hello World");
+app.get('/api/parts',(req,res)=>{
+    console.log(`Get request for ${req.url}`);
+    res.send(parts);
 });
 
 //application should be listening for incoming msgs on this port
