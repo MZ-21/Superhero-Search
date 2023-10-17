b182c003de06f81475242052d5fd10113f8c4ebc
mzekria2 <mzekria2@uwo.ca>
2023-10-17 15:36:21 -0400

    began front-end

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ const parts = [
     {id:102, name:"Belt",colour: "red"},
     {id:103, name:"Hat",colour: "purple"},
 ];
+//setup serving front-end code
+app.use(`/`,express.static('static'))
+
 //using app.get to get url so when someone access that path, something happens
 app.get('/api/parts',(req,res)=>{
     console.log(`Get request for ${req.url}`);
     res.send(parts);
 });
+
+
 
 //application should be listening for incoming msgs on this port
 app.listen(port,()=>{
