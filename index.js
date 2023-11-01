ff1bd0a484d1c2b6e349049ce4c97f222d3f5724
mzekria2 <mzekria2@uwo.ca>
2023-11-01 17:20:49 -0400

    added front-end

--- index.js
+++ index.js
@@ -24,67 +24,79 @@ app.use((req, res, next) => {//for all the routes. Calls callback function for e
 //parse data in body as json onto router object using middleware
 router.use(express.json()); 
 
+//grouping routes based on prefix
+router.route('/')//all routes to base prefix
+    .get((req,res)=>{//get a list of parts
+        res.send(parts);
 
-//using app.get to get url so when someone access that path, something happens
-router.get('/',(req,res)=>{
-    res.send(parts);
-});
+    })
 
-//using get request to get info for a part
-router.get('/:part_id',(req,res)=>{
-    const id = req.params.part_id;
-    const part = parts.find(p => p.id === parseInt(id)); 
-    //if the id in the object === to one in request. checking type = as well. input sanitization when converting id to int
-    if(part){
-        res.send(part);
-    }
-    else {
-        //if the id doesnt match, there is an error and the part wasnt found
-        res.status(404).send(`Part ${id} was not found!`);
-    }
-});
+    //create a part
+    .post((req,res)=>{
+        const newpart = req.body;
+        newpart.id = 100 + parts.length;
+        if(newpart.name){
+            parts.push(newpart);
+            res.send(newpart);
 
-//create/replace part data for a given id using put
-router.put('/:id',(req,res)=>{
-    const newpart = req.body;
-    console.log("Part: ", newpart);
+        }
+        else {
+            res.status(400).send('Missing name');
+        }
+    })
+router.route('/:id')
+    .get((req,res)=>{//using get request to get info for a part
+        const part = parts.find(p => p.id === parseInt(req.params.id)); 
+        //if the id in the object === to one in request. checking type = as well. input sanitization when converting id to int
+        if(part){
+            res.send(part);
+        }
+        else {
+            //if the id doesnt match, there is an error and the part wasnt found
+            res.status(404).send(`Part ${req.params.id} was not found!`);
+        }
+    })
+    //create/replace part data for a given id using put
+    .put((req,res)=>{
+        const newpart = req.body;
+        console.log("Part: ", newpart);
 
-    //add id field
-    newpart.id = parseInt(req.params.id);
+        //add id field
+        newpart.id = parseInt(req.params.id);
 
-    //replacing part with new one
-    const part = parts.findIndex(p => p.id === parseInt(newpart.id));
-    if(part < 0 ){//not found
-        console.log("Creating new part");
-        parts.push(newpart);
-    }
-    else {
-        console.log("Modifying part", req.params.id);
-        parts[part] = newpart;
-    } 
-    res.send(newpart);
-});
+        //replacing part with new one
+        const part = parts.findIndex(p => p.id === parseInt(newpart.id));
+        if(part < 0 ){//not found
+            console.log("Creating new part");
+            parts.push(newpart);
+        }
+        else {
+            console.log("Modifying part", req.params.id);
+            parts[part] = newpart;
+        } 
+        res.send(newpart);
+    })
+    //Update stock level
+    .post((req,res)=>{
+        const newpart = req.body;
+        console.log("Part: ", newpart);
 
-//Update stock level
-router.post('/:id',(req,res) => {
-    const newpart = req.body;
-    console.log("Part: ", newpart);
+        //add id field
+        newpart.id = parseInt(req.params.id);
 
-    //add id field
-    newpart.id = parseInt(req.params.id);
+        //replacing part with new one
+        const part = parts.findIndex(p => p.id === parseInt(req.params.id));
+        if(part < 0 ){//not found
+            res.status(404).send(`Part ${req.params.id} not found`);
+        }
+        else {
+            console.log("Changing stock for ", req.params.id);
+            parts[part].stock += parseInt(req.body.stock);//stock property must exist
+            res.send(parts[part]);
+        } 
+        res.send(newpart);
+    })
 
-    //replacing part with new one
-    const part = parts.findIndex(p => p.id === parseInt(newpart.id));
-    if(part < 0 ){//not found
-        res.status(404).send(`Part ${req.params.id} not found`);
-    }
-    else {
-        console.log("Changing stock for ", req.params.id);
-        parts[part].stock += parseInt(req.body.stock);//stock property must exist
-        res.send(req.body);
-    } 
-    res.send(newpart);
-});
 
 
 //installing the router at /api/parts
