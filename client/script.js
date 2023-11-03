b2fcbd1c79cec4a96c8fb6a0e5c20f5fdb90a3c5
mzekria2 <mzekria2@uwo.ca>
2023-11-03 15:30:43 -0400

    created routes

--- client/script.js
+++ client/script.js
@@ -1,50 +1,57 @@
-document.getElementById('get-inventory').addEventListener('click',getInventory);
-getInventory();
-function getInventory() {
-    fetch("/api/parts") 
-    .then(res => res.json()
-    .then(data => {
-        console.log(data);
-        const l = document.getElementById('inventory');
+document.getElementById("search").addEventListener('click',getSuperheroById)
 
-        data.forEach(e => {
-            const item = document.createElement('li');
-            item.appendChild(document.createTextNode(`${e.name} (${e.colour}) Qty: ${e.stock}`));
-            l.appendChild(item);
-        })
-    })
-    )
-}
+// function getSuperheroById(){
+//     fetch("/api/Superheroes"){
+        
+//     }
+// }
+// document.getElementById('get-inventory').addEventListener('click',getInventory);
+// getInventory();
+// function getInventory() {
+//     fetch("/api/parts") 
+//     .then(res => res.json()
+//     .then(data => {
+//         console.log(data);
+//         const l = document.getElementById('inventory');
 
-function addInventory(){
-    const newpart = {
-        name: document.getElementById('name').value,
-        colour: document.getElementById('colour').value,
-        stock: document.getElementById('stock').value
-    }
-    console.log(newpart);
-    fetch('/api/parts', {//first parameter is a url
-        method: 'POST',
-        headers: {'Content-type': 'application/json'},
-        body: JSON.stringify(newpart)
+//         data.forEach(e => {
+//             const item = document.createElement('li');
+//             item.appendChild(document.createTextNode(`${e.name} (${e.colour}) Qty: ${e.stock}`));
+//             l.appendChild(item);
+//         })
+//     })
+//     )
+// }
 
-    })//adding headers
-    .then(res => {
-        if(res.ok) {
-            res.json()
-            .then(data => {
-                console.log(data)
-                getInventory();
-                document.getElementById('status').innerText = `Created part ${data.id}: ${data.name}`;
-            })
-            .catch(err => console.log("Failed to get json object"))
-        }
-        else {
-            console.log("Error: ", res.status);
-            document.getElementById('status').innerText = 'Failed to add item';
-        }
-    })
-    .catch()
-}
+// function addInventory(){
+//     const newpart = {
+//         name: document.getElementById('name').value,
+//         colour: document.getElementById('colour').value,
+//         stock: document.getElementById('stock').value
+//     }
+//     console.log(newpart);
+//     fetch('/api/parts', {//first parameter is a url
+//         method: 'POST',
+//         headers: {'Content-type': 'application/json'},
+//         body: JSON.stringify(newpart)
 
-document.getElementById("additem").addEventListener("click",addInventory);
+//     })//adding headers
+//     .then(res => {
+//         if(res.ok) {
+//             res.json()
+//             .then(data => {
+//                 console.log(data)
+//                 getInventory();
+//                 document.getElementById('status').innerText = `Created part ${data.id}: ${data.name}`;
+//             })
+//             .catch(err => console.log("Failed to get json object"))
+//         }
+//         else {
+//             console.log("Error: ", res.status);
+//             document.getElementById('status').innerText = 'Failed to add item';
+//         }
+//     })
+//     .catch()
+// }
+
+// document.getElementById("additem").addEventListener("click",addInventory);
--- server/index.js
+++ server/index.js
@@ -1,103 +1,65 @@
 const express = require('express'); //importing module from dependencies using require
-const low = require('lowdb');
-const app = express();
+const app = express();//used to configure server
+const path = require('path'); //path module to manipulate file paths
+const fs = require('fs'); //module for files
+const mongoose = require('mongoose');
 
-const db = low(new FileSync('superheroes/superhero_info.json')); //creating and intializing a lowdb instance, connecting to a json file 
-const port = process.env.PORT || 3000;
+//url for db
+const dbURL = 'mongodb://localhost/superheroes/superhero_info.json';
+//connecting to db
+mongoose.connect(dbURL);
+const db = mongoose.connection;
+db.on('error',(error)=> {console.error(error)})
+db.once('open', ()=> console.log("Connected"))
+
+
+const port = 5000;
 const router = express.Router(); //route object
 
+//defining path
+const joinedPath = path.join(__dirname, '../client');
 
+//reading from json 
+const filePathToInfo = "server/superheroes/superhero_info.json";
 
-//setup serving front-end code
-app.use(`/`,express.static('static'))
-
-//setup middleware to do logging (function inserted into processing pipeline) ->fixes redundency 
-app.use((req, res, next) => {//for all the routes. Calls callback function for every request
-    console.log(`${req.method} request for ${req.url}`);
-    next(); //next allows to continue processing
+var parsedData = []; //array to hold superhero data
+fs.readFile(filePathToInfo, 'utf-8', (err,data)=>{
+    if(err) {
+        console.error('Error reading the file:',err);
+        return;
+    }
+    try {
+        var parsed = JSON.parse(data);
+        for(object of parsed){
+            parsedData.push(object);
+        }
+        
+    } catch (error){
+        console.error('Error parsing json',error)
+    }
+    
 });
 
+
+
+//setup serving front-end code (middleware using use - middleware is when the server gets a request but before passed to route  )
+app.use('/',express.static(joinedPath)); 
+
 //parse data in body as json onto router object using middleware
-router.use(express.json()); 
+app.use(express.json()); //no longer need json.parse in each req
 
-//grouping routes based on prefix
-router.route('/')//all routes to base prefix
-    .get((req,res)=>{//get a list of parts
-        res.send(parts);
+//for html
+app.get('/',(req,res)=>{
+    res.sendFile('index.html', { root: joinedPath });
+})
 
-    })
+//getting based on id
+router.get('/:id',(req,res) =>{
+    res.send();
+})
 
-    //create a part
-    .post((req,res)=>{
-        const newpart = req.body;
-        newpart.id = 100 + parts.length;
-        if(newpart.name){
-            parts.push(newpart);
-            res.send(newpart);
-
-        }
-        else {
-            res.status(400).send('Missing name');
-        }
-    })
-router.route('/:id')
-    .get((req,res)=>{//using get request to get info for a part
-        const part = parts.find(p => p.id === parseInt(req.params.id)); 
-        //if the id in the object === to one in request. checking type = as well. input sanitization when converting id to int
-        if(part){
-            res.send(part);
-        }
-        else {
-            //if the id doesnt match, there is an error and the part wasnt found
-            res.status(404).send(`Part ${req.params.id} was not found!`);
-        }
-    })
-    //create/replace part data for a given id using put
-    .put((req,res)=>{
-        const newpart = req.body;
-        console.log("Part: ", newpart);
-
-        //add id field
-        newpart.id = parseInt(req.params.id);
-
-        //replacing part with new one
-        const part = parts.findIndex(p => p.id === parseInt(newpart.id));
-        if(part < 0 ){//not found
-            console.log("Creating new part");
-            parts.push(newpart);
-        }
-        else {
-            console.log("Modifying part", req.params.id);
-            parts[part] = newpart;
-        } 
-        res.send(newpart);
-    })
-    //Update stock level
-    .post((req,res)=>{
-        const newpart = req.body;
-        console.log("Part: ", newpart);
-
-        //add id field
-        newpart.id = parseInt(req.params.id);
-
-        //replacing part with new one
-        const part = parts.findIndex(p => p.id === parseInt(req.params.id));
-        if(part < 0 ){//not found
-            res.status(404).send(`Part ${req.params.id} not found`);
-        }
-        else {
-            console.log("Changing stock for ", req.params.id);
-            parts[part].stock += parseInt(req.body.stock);//stock property must exist
-            res.send(parts[part]);
-        } 
-        res.send(newpart);
-    })
-
-
-
-//installing the router at /api/parts
-app.use('/api/parts',router); //used for this prefix
-
+//installing the router
+app.use('/api/superheroes',router);
 
 //application should be listening for incoming msgs on this port
 app.listen(port,()=>{
