1b02bb2126dc1a3be2c2e260621836e3b8406d1d
mzekria2 <mzekria2@uwo.ca>
2023-11-05 11:06:32 -0500

    display publishers

--- client/script.js
+++ client/script.js
@@ -1,57 +1,96 @@
-document.getElementById("search").addEventListener('click',getSuperheroById)
 
-// function getSuperheroById(){
-//     fetch("/api/Superheroes"){
+const routerPath = "/api/superheroes";
+function getSuperheroById(){
+    var idToReq = document.getElementById("id-input").value;
+    var parsed = parseInt(idToReq);
+    
+    fetch(`/api/superheroes/${parsed}`) 
+        .then(res => res.json() //used to send JSON response, pass json object as arguement, converted to json string 
+        .then(data => {
+                var divForSearchID = document.createElement('div');
+                var ul = document.createElement('ul');
+                var li = document.createElement('li');
+                ul.className = "list-heroes"
+                li.className = "superhero";
+                var headerName = document.createElement('h2');
+                var pForInfo = document.createElement('p');
+                headerName.textContent = `${data.name}`;
+                pForInfo.textContent = `Gender:${data.Gender} Eye color:${data['Eye color']} Race:${data.Race} Hair Colour:${data['Hair color']} Height:${data.Height} Publisher:${data.Publisher} Skin colour:${data['Skin color']} Alignment:${data.Alignment} Weight:${data.Weight}`;
+                li.appendChild(headerName);
+                li.appendChild(pForInfo);
+                ul.appendChild(li);
+                divForSearchID.appendChild(ul);
+                document.getElementById("result").appendChild(divForSearchID);      
+            
+        })
+        .catch((e)=>{
+            console.log(e)
+        })
+            
+        )
+        .catch(e => console.log(e))
         
-//     }
-// }
-// document.getElementById('get-inventory').addEventListener('click',getInventory);
-// getInventory();
-// function getInventory() {
-//     fetch("/api/parts") 
-//     .then(res => res.json()
-//     .then(data => {
-//         console.log(data);
-//         const l = document.getElementById('inventory');
+}
 
-//         data.forEach(e => {
-//             const item = document.createElement('li');
-//             item.appendChild(document.createTextNode(`${e.name} (${e.colour}) Qty: ${e.stock}`));
-//             l.appendChild(item);
-//         })
-//     })
-//     )
-// }
+function getSuperHeroPower(){
+    var idToReq = document.getElementById("id-input").value;
+    var parsed = parseInt(idToReq);
+    
+    fetch(`/api/superheroes/${parsed}/powers`)
+        .then(res => res.json()
+        .then(dataPower => {
+            var divResult = document.getElementById("result");
+            var divForPowers = document.createElement('div');
+            var headerForPowers = document.createElement('h2');
+            var powers = document.createElement('p');
+            headerForPowers.textContent = "POWERS:"; 
+            divResult.innerHTML = "";
+            powers.textContent = "";
+            for(value in dataPower){
+                if(dataPower[value] != "False" && value != "hero_names"){
+                    
+                    powers.textContent += `${value}, `;
+                    
+                }
+                // console.log(data)
+            }
+            divForPowers.appendChild(headerForPowers);
+            divForPowers.appendChild(powers);
+            divResult.appendChild(divForPowers);
 
-// function addInventory(){
-//     const newpart = {
-//         name: document.getElementById('name').value,
-//         colour: document.getElementById('colour').value,
-//         stock: document.getElementById('stock').value
-//     }
-//     console.log(newpart);
-//     fetch('/api/parts', {//first parameter is a url
-//         method: 'POST',
-//         headers: {'Content-type': 'application/json'},
-//         body: JSON.stringify(newpart)
+        })
+        )
 
-//     })//adding headers
-//     .then(res => {
-//         if(res.ok) {
-//             res.json()
-//             .then(data => {
-//                 console.log(data)
-//                 getInventory();
-//                 document.getElementById('status').innerText = `Created part ${data.id}: ${data.name}`;
-//             })
-//             .catch(err => console.log("Failed to get json object"))
-//         }
-//         else {
-//             console.log("Error: ", res.status);
-//             document.getElementById('status').innerText = 'Failed to add item';
-//         }
-//     })
-//     .catch()
-// }
+}
 
-// document.getElementById("additem").addEventListener("click",addInventory);
+function getPublishers(){
+    fetch(`${routerPath}/publisher`)
+    .then(res => res.json()
+    .then(publisherData => {
+        var divResult = document.getElementById("result");
+        var divPublishers = document.createElement('div');
+        var publisherHeader = document.createElement('h2');
+        var ulP = document.createElement('ul');
+        ulP.className = "list-publishers";
+        publisherHeader.className = "publisher-header";
+        publisherHeader.textContent = "PUBLISHERS: ";
+
+        for(publisher of publisherData){
+            var liP = document.createElement('li');
+            liP.textContent = `${publisher}`;
+            ulP.appendChild(liP);
+        }
+        divPublishers.appendChild(publisherHeader);
+        divPublishers.appendChild(ulP);
+        divResult.appendChild(divPublishers);
+
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+    )
+}
+document.getElementById("id-btn-info").addEventListener('click',getSuperheroById);
+document.getElementById("id-btn-power").addEventListener('click',getSuperHeroPower);
+document.getElementById("publishers-btn").addEventListener('click',getPublishers);
+
--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ const express = require('express'); //importing module from dependencies using r
 const app = express();//used to configure server
 const path = require('path'); //path module to manipulate file paths
 const fs = require('fs'); //module for files
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-//url for db
-const dbURL = 'mongodb://localhost/superheroes/superhero_info.json';
-//connecting to db
-mongoose.connect(dbURL);
-const db = mongoose.connection;
-db.on('error',(error)=> {console.error(error)})
-db.once('open', ()=> console.log("Connected"))
+// //url for db
+// const dbURL = 'mongodb://localhost/superheroes/superhero_info.json';
+// //connecting to db
+// mongoose.connect(dbURL);
+// const db = mongoose.connection;
+// db.on('error',(error)=> {console.error(error)})
+// db.once('open', ()=> console.log("Connected"))
 
 
 const port = 5000;
@@ -19,44 +19,126 @@ const router = express.Router(); //route object
 //defining path
 const joinedPath = path.join(__dirname, '../client');
 
-//reading from json 
-const filePathToInfo = "server/superheroes/superhero_info.json";
-
-var parsedData = []; //array to hold superhero data
-fs.readFile(filePathToInfo, 'utf-8', (err,data)=>{
-    if(err) {
-        console.error('Error reading the file:',err);
-        return;
-    }
-    try {
-        var parsed = JSON.parse(data);
-        for(object of parsed){
-            parsedData.push(object);
-        }
-        
-    } catch (error){
-        console.error('Error parsing json',error)
-    }
-    
-});
-
-
-
 //setup serving front-end code (middleware using use - middleware is when the server gets a request but before passed to route  )
 app.use('/',express.static(joinedPath)); 
 
 //parse data in body as json onto router object using middleware
 app.use(express.json()); //no longer need json.parse in each req
 
+
 //for html
 app.get('/',(req,res)=>{
     res.sendFile('index.html', { root: joinedPath });
-})
+});
+
+//reading from json 
+const filePathToInfo = "superheroes/superhero_info.json";
+const filePathPowers = "superheroes/superhero_powers.json";
+
+
+//getting publishers
+router.route('/publisher')
+    .get((req,res)=>{
+        console.log("err")
+        fs.readFile(filePathToInfo, 'utf-8', (err,heroData)=>{
+            try {
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    var parseHeroes = JSON.parse(heroData);
+                    const publishers = []
+                    console.log(parseHeroes)
+                    for(h of parseHeroes){
+                        if(!publishers.includes(h.Publisher) && h.Publisher!=""){
+                            publishers.push(h.Publisher);
+                        }
+                    }
+                    if(publishers.length > 0 ){
+                        res.send(publishers);
+                    }
+                }
+            }
+            catch (error){
+                res.status(500).send(`server unable to fulfill request!`);
+            } 
+        })
+    });
+
+router.route('/:id')
+    .get((req,res) =>{
+        fs.readFile(filePathToInfo, 'utf-8', (err,data)=>{
+            try {
+                var parsedHeroes = JSON.parse(data);
+                for(hero of parsedHeroes){
+                    // console.log(typeof(hero.id),typeof(req.params.id))
+                    if(parseInt(hero.id) === parseInt(req.params.id)){
+                        res.send(hero);
+                        // found=true;
+                        // break;
+                    }
+                }
+            }
+            catch (error){
+                res.status(500).send(`server unable to fulfill request!`);
+            } 
+        })
+    });
+    
+
+//getting powers based on ID
+router.route('/:id/powers')
+    .get((req,res)=>{
+        const id = req.params.id;
+        fs.readFile(filePathToInfo, 'utf-8', (err,data2)=>{
+            if(err){
+                console.log(err);
+            }
+            try {
+                var superheroes = JSON.parse(data2);
+                for(hero of superheroes){
+                    if(parseInt(hero.id) === parseInt(id)){
+                            readPowersFile(hero).then((powers)=>{
+                            found = true;
+                            console.log(found)
+                            res.send(powers);
+                           
+                        }).catch((error2)=> {
+                            console.log(error2)
+                        })         
+                    }
+                }
+               
+            }
+            catch (error){     
+                res.status(500).send(`server unable to fulfill request! ${error}`);
+            } 
+        })
+    });
+
 
 //getting based on id
-router.get('/:id',(req,res) =>{
-    res.send();
-})
+
+function readPowersFile(hero){//function to read from power file
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePathPowers, 'utf-8', (err, powerData) => {
+            if (err) {
+                reject(err);
+            } else {
+                var parsedPowerData = JSON.parse(powerData);
+                var powerFound;
+                for (power of parsedPowerData) {
+                    if (String(hero.name) === String(power.hero_names)) {
+                        powerFound = power;
+                    }
+                }
+                resolve(powerFound);
+            }
+        });
+    });
+}
+
+
 
 //installing the router
 app.use('/api/superheroes',router);
