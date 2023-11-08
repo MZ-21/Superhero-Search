035a527dbd777fcb142a3481628894c2c4d32276
mzekria2 <mzekria2@uwo.ca>
2023-11-08 14:23:38 -0500

    uninstalled joi

--- models/model.js
+++ models/model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-//import Joi from 'joi';
+
 
 const heroSchema = new mongoose.Schema({
    listN:{
@@ -23,20 +23,4 @@ const heroSchema = new mongoose.Schema({
         }
    ]
 })
-
-// const schemaVal = 
-// Joi.object({
-//     id: Joi.string(),
-//     name: Joi.string(),
-//     Gender: Joi.string(),
-//     Eyecolor: Joi.string(),
-//     Race: Joi.string(),
-//     Haircolor: Joi.string(),
-//     Height: Joi.string(),
-//     Publisher: Joi.string(),
-//     Skincolor: Joi.string(),
-//     Alignment: Joi.string(),
-//     Weight: Joi.string(),
-//     Powers: Joi.string()   
-// })
 module.exports = mongoose.model('HeroList', heroSchema);
